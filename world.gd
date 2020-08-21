ed2c4263a9ba24e1dc48cf822b4eca3593365e7e
Gregory Assaad <gregory@assaad.us>
2020-08-21 18:13:54 -0500

    Add a commit message

--- world.gd
+++ world.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-onready var loadingbar = $UI/LoadingScreen
 var level_thread = Thread.new()
 var load_percent = 0
 
@@ -9,3 +8,13 @@ func _ready():
 
 func load_game():
 	level_thread.start(self, "build_level", null , 1)
+	setup_character()
+
+func setup_character():
+	load_percent += 50
+	$LoadingScreen.update_percent(load_percent)
+
+func build_level(empty):
+	load_percent += 50
+	$LoadingScreen.update_percent(load_percent)
+	level_thread.wait_to_finish()
